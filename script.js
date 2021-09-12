49a51595c56dff182ac0d649252a78e85f465b0b
Petro Holobutovskyy <p.holobutowski@gmail.com>
2021-09-12 17:40:44 +0200

    functionality to show number of jumps added

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 const gameOverText = document.querySelector(".gameOverWarning");
+const game = document.querySelector(".game");
+const text = document.querySelector(".text")
+const resultInfo = document.querySelector(".resultInfo")
+let singleJump = 0;
 
 document.addEventListener("keydown", function(event) {
     jump();
+    
+    singleJump++;
+    console.log(singleJump);
+    // const numberOfJumps = document.createElement("p").innerText= `Number of jumps: ${singleJump}`;
+    // game.appendChild(numberOfJumps)
+    text.innerHTML =`Number of jumps: ${singleJump}`
+
 });
 
+
 function jump() {
+    
   if(dino.classList != 'jump') {
       dino.classList.add('jump');
   }
@@ -23,7 +36,10 @@ let isAlive = setInterval(function(){
         cactus.style.animation ="none";
         cactus.style.display ="none";
         gameOverText.classList.remove("hidden");
-        gameOverText.classList.add("active")
-        // alert("Przegraes:(");
+        gameOverText.classList.add("active");
+        resultInfo.innerHTML =  `Your result: ${singleJump}`;
+       
+        text.classList.add("hidden")
+        
     }
 }, 100);
